fc0c05c2a133d163757a9afdd36743724e72b429
KaesDingeling <kingdingeling721@gmail.com>
2020-05-05 23:44:59 +0200

    update static login test

--- src/test/java/de/kaesdingeling/springtestexample/test/TestLoginApi.java
+++ src/test/java/de/kaesdingeling/springtestexample/test/TestLoginApi.java
@@ -1,6 +1,5 @@
 package de.kaesdingeling.springtestexample.test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.commons.lang3.StringUtils;
@@ -8,9 +7,11 @@ import org.junit.jupiter.api.Test;
 
 import com.google.gson.Gson;
 
+import de.kaesdingeling.springtestexample.data.ResponsePOSTMono;
+import de.kaesdingeling.springtestexample.data.models.ConfigData;
 import de.kaesdingeling.springtestexample.data.models.LoginRequestData;
 import de.kaesdingeling.springtestexample.data.models.ProfileData;
-import de.kaesdingeling.springtestexample.rest.client.RESTClientLogin;
+import de.kaesdingeling.springtestexample.rest.client.RESTClient;
 import de.kaesdingeling.springtestexample.test.utils.MockServerUtils;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
@@ -33,7 +34,8 @@ public class TestLoginApi {
 							
 							if (loginData != null && StringUtils.equals(loginData.getUsername(), "admin") && StringUtils.equals(loginData.getPassword(), "bambus123")) {
 								return MockServerUtils.createResponse(200, new Gson().toJson(ProfileData.builder()
-										.name("nö")
+										.id("login-test-id")
+										.name("login-test-name")
 										.build()));
 							}
 						} else {
@@ -46,9 +48,15 @@ public class TestLoginApi {
 			}
 		});
 		
-		ProfileData data = new RESTClientLogin().login("admin", "bambus123");
+		RESTClient restClient = RESTClient.init(ConfigData.builder()
+    			.https(false)
+    			.url("localhost")
+    			.port(8070)
+    			.build());
 		
-		assertNotNull(data);
-		assertEquals(data.getName(), "nö");
+		ResponsePOSTMono<LoginRequestData, ProfileData> loginResponse = restClient.getLogin().login("admin", "bambus123");
+		
+		assertEquals(loginResponse.getResponseData().getId(), "login-test-id");
+		assertEquals(loginResponse.getResponseData().getName(), "login-test-name");
 	}
 }
